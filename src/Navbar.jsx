4c46236a69023243807f862f06273a51b09c72b2
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-10-07 16:14:53 +0400

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,43 +1,44 @@
 import React from "react";
-import Logo from "./pics/Logo.png";
+import Logo from "./assets/pics/Logo.png";
+import Tel from "./assets/pics/tel.png";
+import Mail from "./assets/pics/mail.png";
 
 class Navbar extends React.Component {
     render() {
         return(
             <nav class="navbar navbar-expand-lg fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img className="logo" src={Logo} alt="Logo" style={{width:"60px", height:"60px"}} />
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link navLinks" aria-current="page" href="#">მთავარი</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link navLinks2 ms-3" href="#">ჩემს შესახებ</a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link navLinks3 ms-3">ჩემი პორტფოლიო</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link navLinks4 ms-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            კონტაქტი
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#"></a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+              <div class="container">
+                <a class="navbar-brand" href="#">
+                  <img className="logo" src={Logo} alt="Logo" style={{width:"60px", height:"60px"}} />
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item ms-auto">
+                      <a class="nav-link navLinks ms-3" aria-current="page" href="#">მთავარი</a>
+                    </li>
+                    <li class="nav-item ms-auto">
+                      <a class="nav-link navLinks2 ms-3" href="#">ჩემს შესახებ</a>
+                    </li>
+                    <li class="nav-item ms-auto">
+                      <a href="#" class="nav-link navLinks3 ms-3">ჩემი პორტფოლიო</a>
+                    </li>
+                    <li class="nav-item ms-auto dropdown">
+                      <a class="nav-link navLinks4 ms-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        კონტაქტი
+                      </a>
+                      <ul class="dropdown-menu dropdownmenu707">
+                        <li><a class="dropdown-item a-drop-item" href="#"><img src={Tel} alt="Tel" style={{width:"20px", height: "20px"}} />&nbsp; +995551574747</a></li>
+                        <li><a class="dropdown-item a-drop-item mt-1" href="#"><img src={Mail} alt="Mail" style={{width: "20px", height: "20px"}} />&nbsp; a.saginashvili707@outlook.com</a></li>
+                        <li><a class="dropdown-item a-drop-item" href="#">Something else here</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+          </nav>
         )
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./Navbar";
-import "./css/style.css";
+import "./assets/css/style.css";
 
 class App extends Component {
     render() {
@@ -15,12 +15,22 @@ class App extends Component {
             <div className="container pt-5">
 
             <div className="row pt-5">
+                
+                <div className="col-12">
 
-            <div className="col-6">
+                <h1 className="h1-head">გამარჯობა მსოფლიო!</h1>
+
+                </div>
+
+            <div className="col-6 mt-4">
 
             <div className="w-100">
 
-            <h3></h3>
+            <h3 className="h3-head">ორიოდე სიტყვა ჩემს შესახებ!</h3>
+            <p className="p-paragraph">&nbsp;&nbsp;მოგესალმებით მე გახლავართ ავთანდილ საგინაშვილი, ვმუშაობ FrontEnd დეველოპერად მაქვს რამდენიმე
+                    ნამუშევარი რაც მინდა რომ გაგიზიაროთ. იმედია მოგეწონებათ და თხოვნა მექნება გამოხატოთ რეაქცია.<br />
+                    &nbsp;&nbsp;თუ გსურთ მეტი გაიგოთ ჩემს შესახებ გადადით <a href="#">"ჩემს შესახებ"</a> განყოფილებაში.
+                </p>
 
             </div>
 
