6982dfef3c5fc2e3c482be4bae8b29f638187061
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2024-03-10 17:32:28 +0400

    screen update

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,7 +3,6 @@ import { gsap } from "gsap";
 
 import BlogPic from "./assets/pics/blog.png";
 import CoralPic from "./assets/pics/coral.png";
-import Hounter from "./assets/pics/Hounter.png";
 
 function Carousel() {
   const scrollContainerRef = useRef(null);
@@ -53,7 +52,6 @@ function Carousel() {
                     alt="blogpic"
                     style={{height: "120%" }}
                   />
-                </a>
                 <a href="https://simulation-of-api-test.vercel.app/" target="_blank">
                   <img
                     className="w-100"
@@ -62,14 +60,7 @@ function Carousel() {
                     style={{ height: "120%" }}
                   />
                 </a>
-                  <a href="https://hounter-nine.vercel.app/" target="_blank">
-                  <img
-                    className="w-100"
-                    src={Hounter}
-                    alt="Hounter"
-                    style={{ height: "120%" }}
-                  />
-                  </a>
+                </a>
               </div>
               <div className="screen-2 position-absolute"></div>
               <div className="screen-3 position-absolute"></div>
@@ -82,198 +73,5 @@ function Carousel() {
 }
 
 export default Carousel;
-
-
-
-// import React, { useEffect, useRef } from "react";
-// import { gsap } from "gsap";
-
-// import BlogPic from "./assets/pics/blog.png";
-// import CoralPic from "./assets/pics/coral.png";
-// import Hounter from "./assets/pics/Hounter.png";
-
-// function Carousel() {
-//   const scrollContainerRef = useRef(null);
-
-//   useEffect(() => {
-//     const scrollContainer = scrollContainerRef.current;
-
-//     const handleWindowScroll = () => {
-
-//       const scrollPosition = window.scrollY + window.innerHeight;
-//       const documentHeight = document.documentElement.scrollHeight;
-
-
-//       if (scrollPosition >= documentHeight) {
-
-//         triggerCarouselScroll();
-//       }
-//     };
-
-//     const triggerCarouselScroll = () => {
-
-//       gsap.to(scrollContainer, {
-//         duration: 2,
-//         scrollLeft: "+=200px", 
-//         ease: "back(25)",
-//       });
-//     };
-
-//     window.addEventListener("scroll", handleWindowScroll);
-
-//     return () => {
-//       window.removeEventListener("scroll", handleWindowScroll);
-//     };
-//   }, [scrollContainerRef.current]);
-
-//   return (
-//     <div className="carouselDiv w-100">
-//       <div className="container">
-//         <div className="row pb-5">
-//           <div className="col-3">
-//             <div className="w-100" style={{ height: "500px" }}>
-//               <h6 className="scrollTo">
-//                 _Scroll to <span style={{ color: "white" }}>-----</span>
-//               </h6>
-//             </div>
-//           </div>
-//           <div className="col-9">
-//             <div
-//               className="w-100 position-relative neomorph"
-//               style={{ perspective: "2000px", height: "450px" }}
-//             >
-//               <div className="screen-1 position-absolute" ref={scrollContainerRef} style={{ overflow: "hidden" }}>
-//                 <a href="https://api-coral-task-wj7e.vercel.app/" target="_blank">
-//                   <img
-//                     className="w-100"
-//                     src={CoralPic}
-//                     alt="blogpic"
-//                     style={{height: "120%" }}
-//                   />
-//                 </a>
-//                 <a href="https://simulation-of-api-test.vercel.app/" target="_blank">
-//                   <img
-//                     className="w-100"
-//                     src={BlogPic}
-//                     alt="blogpic"
-//                     style={{ height: "120%" }}
-//                   />
-//                 </a>
-//                 <a href="https://hounter-nine.vercel.app/" target="_blank">
-//                   <img
-//                     className="w-100"
-//                     src={Hounter}
-//                     alt="Hounter"
-//                     style={{ height: "120%" }}
-//                   />
-//                 </a>
-//               </div>
-//               <div className="screen-2 position-absolute"></div>
-//               <div className="screen-3 position-absolute"></div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Carousel;
-
-
-// import React, { useEffect, useRef } from "react";
-// import { gsap } from "gsap";
-
-// import BlogPic from "./assets/pics/blog.png";
-// import CoralPic from "./assets/pics/coral.png";
-// import Hounter from "./assets/pics/Hounter.png";
-
-// function Carousel() {
-//   const scrollContainerRef = useRef(null);
-
-//   useEffect(() => {
-//     const scrollContainer = scrollContainerRef.current;
-
-//     const handleWindowScroll = () => {
-//       const scrollPosition = window.scrollY + window.innerHeight;
-//       const documentHeight = document.documentElement.scrollHeight;
-
-//       // If the user has scrolled to the bottom of the page
-//       if (scrollPosition >= documentHeight) {
-//         triggerCarouselScroll();
-//       }
-//     };
-
-//     const triggerCarouselScroll = () => {
-//       // Smoothly scroll the carousel to the end
-//       gsap.to(scrollContainer, {
-//         duration: 2,
-//         scrollTo: { x: scrollContainer.scrollWidth, ease: "power2.inOut" },
-//       });
-//     };
-
-//     window.addEventListener("scroll", handleWindowScroll);
-
-//     return () => {
-//       window.removeEventListener("scroll", handleWindowScroll);
-//     };
-//   }, []);
-
-//   return (
-//     <div className="carouselDiv w-100">
-//       <div className="container">
-//         <div className="row pb-5">
-//           <div className="col-3">
-//             <div className="w-100" style={{ height: "500px" }}>
-//               <h6 className="scrollTo">
-//                 _Scroll to <span style={{ color: "white" }}>-----</span>
-//               </h6>
-//             </div>
-//           </div>
-//           <div className="col-9">
-//             <div
-//               className="w-100 position-relative neomorph"
-//               style={{ perspective: "2000px", height: "450px" }}
-//             >
-//               <div
-//                 className="screen-1 position-absolute"
-//                 ref={scrollContainerRef}
-//                 style={{ overflowX: "scroll", overflowY: "hidden", whiteSpace: "nowrap", height: "100%" }}
-//               >
-//                 <a href="https://api-coral-task-wj7e.vercel.app/" target="_blank">
-//                   <img
-//                     className="w-100"
-//                     src={CoralPic}
-//                     alt="Coral"
-//                   />
-//                 </a>
-//                 <a href="https://simulation-of-api-test.vercel.app/" target="_blank">
-//                   <img
-//                     className="w-100"
-//                     src={BlogPic}
-//                     alt="Blog"
-//                   />
-//                 </a>
-//                 <a href="https://hounter-nine.vercel.app/" target="_blank">
-//                   <img
-//                     className="w-100"
-//                     src={Hounter}
-//                     alt="Hounter"
-//                   />
-//                 </a>
-//               </div>
-//               <div className="screen-2 position-absolute"></div>
-//               <div className="screen-3 position-absolute"></div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Carousel;
-
-
 
 
